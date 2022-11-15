6c99c11ab6ff972b6dcf07f6c7ada2989192b868
root <root@ubuntuKDE>
2022-11-15 23:18:13 +0100

    Make examples/vite work (according to readme.md)

--- examples/vite/server/GreeterFuncs.ts
+++
@@ -1,5 +0,0 @@
-export class GreeterFuncs {
-    async greet(name: string) {
-        return `Hello ${name}, greetings from somewhere over the internet !`;
-    }
-}
--- examples/vite/server/index.ts
+++ examples/vite/server/index.ts
@@ -1,33 +1,31 @@
 import express from "express";
 import vite from "vite";
-import {createRESTFuncsHandler} from "@restfuncs/server";
-import {GreeterFuncs} from "./GreeterFuncs.js";
+import {restify} from "@restfuncs/server";
+import {GreeterService} from "./GreeterService.js";
 
 const port = 3000;
 
-(async () => {
-    const app = express();
+const app = express();
 
-    app.use("/api", createRESTFuncsHandler( new GreeterFuncs() ));
+// Remote service(s):
+app.use("/greeterAPI", restify( new GreeterService() ));
 
-    const development = true;
+// Client web:
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/dist')); // Serve pre-built web (cd ../client && npm run build)
+}
+else {
+    // Serve client web through vite dev server:
+    const viteDevServer = await vite.createServer({
+        server: {
+            middlewareMode: 'html'
+        },
+        root: "../client",
+        base: "/",
 
-    if(development) {
-        // Serve client web through vite dev server:
-        const viteDevServer = await vite.createServer({
-            server: {
-                middlewareMode: 'html'
-            },
-            root: "../client",
-            base: "/",
+    });
+    app.use(viteDevServer.middlewares);
+}
 
-        });
-        app.use(viteDevServer.middlewares);
-    }
-    else {
-        app.use(express.static('client/dist')); // Serve pre-built web
-    }
-
-    await app.listen(port);
-    console.log("Server started: http://localhost:" + port)
-})();
+app.listen(port);
+console.log("Server started: http://localhost:" + port)
