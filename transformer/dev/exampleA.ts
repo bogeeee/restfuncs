f2cbb16c9561eac904c0cf7ccb880cd1b7f7c796
Boris Gingold <bogeee@bogitech.de>
2024-02-27 19:27:02 +0100

    - Tried to replace marker with "typia.validate<B>" and do the typia transform as a second transformer -> typia does transform out spot :(

--- transformer/dev/exampleA.ts
+++ transformer/dev/exampleA.ts
@@ -1,7 +1,22 @@
+import typia from "typia"
+function remote() {
+    return function (...args: any[]) {
+        console.log("decorated")
+    }
+}
+type B = {
+    id: number,
+    name: string;
+}
+
 export class A {
     propB: string
 
-    myMethod() {
-        console.log("before")
+    @remote()
+    myMethod(b: B) {
+        "marker"
     }
 }
+
+// A validator that should be replaced by typia:
+//const validator = (obj: unknown) => (typia.validate<B>(obj))
--- transformer/index.ts
+++ transformer/index.ts
@@ -1,4 +1,5 @@
-import type * as ts from 'typescript';
+import * as ts from 'typescript';
+import { factory} from "typescript"
 import type { TransformerExtras, PluginConfig } from 'ts-patch';
 import typescriptRttiTransformer from "typescript-rtti/dist/transformer";
 
@@ -11,21 +12,103 @@ export default function (program: ts.Program, pluginConfig: PluginConfig, { ts: 
     return (ctx: ts.TransformationContext) => {
         return (sourceFile: ts.SourceFile):ts.SourceFile => {
 
-            let typescriptRttiTransformer = typescriptRttiTransformerFactory(ctx);
-            sourceFile = typescriptRttiTransformer(sourceFile); // run it;
+            //let typescriptRttiTransformer = typescriptRttiTransformerFactory(ctx);
+            //sourceFile = typescriptRttiTransformer(sourceFile); // run it;
 
-
+            // @ts-ignore
+            tsInstance.visitNode(sourceFile, hasMarker(tsInstance, ctx));
 
             function visit(node: ts.Node): ts.Node {
-                if (tsInstance.isStringLiteral(node) && node.text === 'before') {
-                    return ctx.factory.createStringLiteral('after');
+                if(tsInstance.isExpressionStatement(node)) {
+                    try {
+                        let found = false;
+                        if (node.getChildren().length > 0) {
+                            const inner = node.getChildAt(0);
+                            if (tsInstance.isStringLiteral(inner) && inner.text === 'marker') {
+                                //return ctx.factory.createStringLiteral('after');
+                                found = true;
+                            }
+                        }
+                        if(found) {
+                            // Insert: const validator = (obj: unknown) => (typia.validate<B>(obj))
+                            return factory.createVariableDeclarationList(
+                                [factory.createVariableDeclaration(
+                                    factory.createIdentifier("validator"),
+                                    undefined,
+                                    undefined,
+                                    factory.createArrowFunction(
+                                        undefined,
+                                        undefined,
+                                        [factory.createParameterDeclaration(
+                                            undefined,
+                                            undefined,
+                                            factory.createIdentifier("obj"),
+                                            undefined,
+                                            (factory.createKeywordTypeNode as anyFn)(ts.SyntaxKind.UnknownKeyword),
+                                            undefined
+                                        )],
+                                        undefined,
+                                        (factory.createToken as anyFn)(ts.SyntaxKind.EqualsGreaterThanToken),
+                                        factory.createParenthesizedExpression(factory.createCallExpression(
+                                            factory.createPropertyAccessExpression(
+                                                factory.createIdentifier("typia"),
+                                                factory.createIdentifier("validate")
+                                            ),
+                                            [factory.createTypeReferenceNode(
+                                                factory.createIdentifier("B"),
+                                                undefined
+                                            )],
+                                            [factory.createIdentifier("obj")]
+                                        ))
+                                    )
+                                )],
+                                ts.NodeFlags.Const | ts.NodeFlags.Const
+                            )
+
+
+
+                        }
+                    }
+                    catch (e) {
+
+                    }
                 }
+
                 return tsInstance.visitEachChild(node, visit, ctx);
             }
             // @ts-ignore
             sourceFile = tsInstance.visitNode(sourceFile, visit);
 
+            // @ts-ignore
+            tsInstance.visitNode(sourceFile, hasMarker(tsInstance, ctx));
+
             return sourceFile;
         };
     };
+}
+
+
+type anyFn = (...args: any[]) => any
+
+
+function hasMarker(tsInstance: any, ctx: any) {
+    return function visit(node: ts.Node): ts.Node {
+        if (tsInstance.isExpressionStatement(node)) {
+            try {
+                if (node.getChildren().length > 0) {
+                    const inner = node.getChildAt(0);
+                    // @ts-ignore
+                    if (tsInstance.isStringLiteral(inner) && inner.text === 'marker') {
+                        //return ctx.factory.createStringLiteral('after');
+
+                        console.log("found marker!");
+                    }
+                }
+            } catch (e) {
+
+            }
+        }
+
+        return tsInstance.visitEachChild(node, visit, ctx);
+    }
 }
