caf1f190f321efd7edecef452ff78d6263373ed6
Boris Gingold <bogeee@bogitech.de>
2024-02-29 10:26:53 +0100

    use a program transformer (allows for 2 passes). Replace a simple marker and let it transform by typia in a second pass

--- transformer/index.ts
+++ transformer/index.ts
@@ -1,120 +1,153 @@
-import * as ts from 'typescript';
-import { factory} from "typescript"
-import type { TransformerExtras, PluginConfig } from 'ts-patch';
-import typescriptRttiTransformer from "typescript-rtti/dist/transformer";
-import typiaTransformer from "typia/lib/transform"
+import ts, {
+    CompilerHost,
+    CompilerOptions,
+    Program,
+    SyntaxKind,
+    TransformationContext,
+    SourceFile,
+    Node,
+    factory
+} from 'typescript';
+import { PluginConfig, ProgramTransformerExtras } from "ts-patch";
+import {} from 'ts-expose-internals'
+import ex = CSS.ex;
 
-/** Changes string literal 'before' to 'after' */
-export default function (program: ts.Program, pluginConfig: PluginConfig, extras: TransformerExtras): ts.TransformerFactory<any> { // TODO: not the proper signature but makes the example work
-    const { ts: tsInstance } = extras;
-
-    const typescriptRttiTransformerFactory = typescriptRttiTransformer(program); // init
-    const typiaTransformerFactory = typiaTransformer(program, pluginConfig as any, extras);
-
-    // Our transformerfactory:
-    return (ctx: ts.TransformationContext) => {
-        return (sourceFile: ts.SourceFile):ts.SourceFile => {
-
-            //let typescriptRttiTransformer = typescriptRttiTransformerFactory(ctx);
-            //sourceFile = typescriptRttiTransformer(sourceFile); // run it;
-
-            // @ts-ignore
-            tsInstance.visitNode(sourceFile, hasMarker(tsInstance, ctx));
-
-            function visit(node: ts.Node): ts.Node {
-                if(tsInstance.isExpressionStatement(node)) {
-                    try {
-                        let found = false;
-                        if (node.getChildren().length > 0) {
-                            const inner = node.getChildAt(0);
-                            if (tsInstance.isStringLiteral(inner) && inner.text === 'marker') {
-                                //return ctx.factory.createStringLiteral('after');
-                                found = true;
-                            }
-                        }
-                        if(found) {
-                            // Insert: const validator = (obj: unknown) => (typia.validate<B>(obj))
-                            return factory.createVariableDeclarationList(
-                                [factory.createVariableDeclaration(
-                                    factory.createIdentifier("validator"),
-                                    undefined,
-                                    undefined,
-                                    factory.createArrowFunction(
-                                        undefined,
-                                        undefined,
-                                        [factory.createParameterDeclaration(
-                                            undefined,
-                                            undefined,
-                                            factory.createIdentifier("obj"),
-                                            undefined,
-                                            (factory.createKeywordTypeNode as anyFn)(ts.SyntaxKind.UnknownKeyword),
-                                            undefined
-                                        )],
-                                        undefined,
-                                        (factory.createToken as anyFn)(ts.SyntaxKind.EqualsGreaterThanToken),
-                                        factory.createParenthesizedExpression(factory.createCallExpression(
-                                            factory.createPropertyAccessExpression(
-                                                factory.createIdentifier("typia"),
-                                                factory.createIdentifier("validate")
-                                            ),
-                                            [factory.createTypeReferenceNode(
-                                                factory.createIdentifier("B"),
-                                                undefined
-                                            )],
-                                            [factory.createIdentifier("obj")]
-                                        ))
-                                    )
-                                )],
-                                ts.NodeFlags.Const | ts.NodeFlags.Const
-                            )
+// From: https://github.com/nonara/ts-patch/discussions/29#discussioncomment-325979
 
 
+/* ****************************************************************************************************************** */
+// region: Helpers
+/* ****************************************************************************************************************** */
 
-                        }
-                    }
-                    catch (e) {
+/**
+ * Patches existing Compiler Host (or creates new one) to allow feeding updated file content from cache
+ */
+function getPatchedHost(
+    maybeHost: CompilerHost | undefined,
+    tsInstance: typeof ts,
+    compilerOptions: CompilerOptions
+): CompilerHost & { fileCache: Map<string, SourceFile> }
+{
+    const fileCache = new Map();
+    const compilerHost = maybeHost ?? tsInstance.createCompilerHost(compilerOptions, true);
+    const originalGetSourceFile = compilerHost.getSourceFile;
 
-                    }
-                }
+    return Object.assign(compilerHost, {
+        getSourceFile(fileName: string, languageVersion: ts.ScriptTarget) {
+            fileName = tsInstance.normalizePath(fileName);
+            if (fileCache.has(fileName)) return fileCache.get(fileName);
 
-                return tsInstance.visitEachChild(node, visit, ctx);
-            }
-            // @ts-ignore
-            sourceFile = tsInstance.visitNode(sourceFile, visit);
-
-            // @ts-ignore
-            tsInstance.visitNode(sourceFile, hasMarker(tsInstance, ctx));
-
-            let typiaTransformer = typiaTransformerFactory(ctx);
-            sourceFile = typiaTransformer(sourceFile); // run it;
+            const sourceFile = originalGetSourceFile.apply(void 0, Array.from(arguments) as any);
+            fileCache.set(fileName, sourceFile);
 
             return sourceFile;
-        };
-    };
+        },
+        fileCache
+    });
 }
 
+// endregion
 
-type anyFn = (...args: any[]) => any
+
+/* ****************************************************************************************************************** */
+// region: Program Transformer
+/* ****************************************************************************************************************** */
+
+export default function transformProgram(
+    program: Program,
+    host: CompilerHost | undefined,
+    config: PluginConfig,
+    extras?: ProgramTransformerExtras,
+): Program {
+    if(!extras) {
+        throw new Error(`Please add the flag "transformProgram": true to the transformer inside tsconfig.json`);
+    }
+
+    const { ts: tsInstance } = extras;
+    const compilerOptions = program.getCompilerOptions();
+    const compilerHost = getPatchedHost(host, tsInstance, compilerOptions);
+    const rootFileNames = program.getRootFileNames().map(tsInstance.normalizePath);
+
+    /* Transform AST */
+    const transformedSource = tsInstance.transform(
+        /* sourceFiles */ program.getSourceFiles().filter(sourceFile => rootFileNames.includes(sourceFile.fileName)),
+        /* transformers */ [ transformAst.bind(tsInstance) ],
+        compilerOptions
+    ).transformed;
+
+    /* Render modified files and create new SourceFiles for them to use in host's cache */
+    const { printFile } = tsInstance.createPrinter();
+    for (const sourceFile of transformedSource) {
+        const { fileName, languageVersion } = sourceFile;
+        const updatedSourceFile = tsInstance.createSourceFile(fileName, printFile(sourceFile), languageVersion);
+        compilerHost.fileCache.set(fileName, updatedSourceFile);
+    }
+
+    /* Re-create Program instance */
+    return tsInstance.createProgram(rootFileNames, compilerOptions, compilerHost);
+}
+
+// endregion
 
 
-function hasMarker(tsInstance: any, ctx: any) {
-    return function visit(node: ts.Node): ts.Node {
-        if (tsInstance.isExpressionStatement(node)) {
-            try {
-                if (node.getChildren().length > 0) {
-                    const inner = node.getChildAt(0);
-                    // @ts-ignore
-                    if (tsInstance.isStringLiteral(inner) && inner.text === 'marker') {
-                        //return ctx.factory.createStringLiteral('after');
+/* ****************************************************************************************************************** */
+// region: AST Transformer
+/* ****************************************************************************************************************** */
 
-                        console.log("found marker!");
-                    }
-                }
-            } catch (e) {
+/**
+ * Change all 'number' keywords to 'string'
+ *
+ * @example
+ * // before
+ * type A = number
+ *
+ * // after
+ * type A = string
+ */
+function transformAst(this: typeof ts, context: TransformationContext) {
+    const tsInstance = this;
 
+    /* Transformer Function */
+    return (sourceFile: SourceFile) => {
+        return tsInstance.visitEachChild(sourceFile, visit, context);
+
+        /* Visitor Function */
+        function visit(node: Node): Node {
+            if (node.kind === SyntaxKind.StringLiteral && node.text === "marker") {
+                const factory = context.factory;
+                return factory.createIdentifier("validator"),
+                        undefined,
+                        undefined,
+                        factory.createArrowFunction(
+                            undefined,
+                            undefined,
+                            [factory.createParameterDeclaration(
+                                undefined,
+                                undefined,
+                                factory.createIdentifier("obj"),
+                                undefined,
+                                (factory.createKeywordTypeNode as anyFn)(ts.SyntaxKind.UnknownKeyword),
+                                undefined
+                            )],
+                            undefined,
+                            (factory.createToken as anyFn)(ts.SyntaxKind.EqualsGreaterThanToken),
+                            factory.createParenthesizedExpression(factory.createCallExpression(
+                                factory.createPropertyAccessExpression(
+                                    factory.createIdentifier("typia"),
+                                    factory.createIdentifier("validate")
+                                ),
+                                [factory.createTypeReferenceNode(
+                                    factory.createIdentifier("B"),
+                                    undefined
+                                )],
+                                [factory.createIdentifier("obj")]
+                            ))
+                        )
             }
+            else
+                return tsInstance.visitEachChild(node, visit, context);
         }
-
-        return tsInstance.visitEachChild(node, visit, ctx);
     }
 }
+
+// endregion
