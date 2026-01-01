7251e37d4737169a5360b96585cb315eb8168e66
Boris Gingold <bogeee@bogitech.de>
2026-01-02 00:07:57 +0100

    visitReplace: Support sets and maps

--- common/util.test.ts
+++ common/util.test.ts
@@ -12,6 +12,66 @@ describe('visitReplace', function() {
         expect(result).toStrictEqual({a: 1, b: {c: 'replaced', d: {e: 3, f: 4}}, g: 5, h: 'replaced'});
     });
 
+    it('should replace certain values and stop recursion - sets', function() {
+        const target = {a: 1, b: new Set([1,2,3])};
+
+        const result = visitReplace(target, (value, visitChilds, context) => {
+            return value === 2 ? 'replaced' : visitChilds(value, context)
+        });
+
+        expect(result).toStrictEqual({a: 1, b: new Set([1,"replaced",3])});
+    });
+
+    it('should replace certain values and stop recursion - sets 2', function() {
+        const target = {a: 1, b: new Set([1, {x: 2},3])};
+
+        const result = visitReplace(target, (value, visitChilds, context) => {
+            return value === 2 ? 'replaced' : visitChilds(value, context)
+        });
+
+        expect(result).toStrictEqual({a: 1, b: new Set([1,{x:"replaced"},3])});
+    });
+
+    it('should replace certain values and stop recursion - maps - values ', function() {
+        const target = {a: 1, b: new Map<any, any>([["a",1], ["b", 2], ["c", 3]])};
+
+        const result = visitReplace(target, (value, visitChilds, context) => {
+            return value === 2 ? 'replaced' : visitChilds(value, context)
+        });
+
+        expect(result).toStrictEqual({a: 1, b: new Map<any, any>([["a",1], ["b", "replaced"], ["c", 3]])});
+    });
+
+    it('should replace certain values and stop recursion - maps - values 2', function() {
+        const target = {a: 1, b: new Map<any, any>([["a",1], ["b", {x:2}], ["c", 3]])};
+
+        const result = visitReplace(target, (value, visitChilds, context) => {
+            return value === 2 ? 'replaced' : visitChilds(value, context)
+        });
+
+        expect(result).toStrictEqual({a: 1, b: new Map<any, any>([["a",1], ["b", {x: "replaced"}], ["c", 3]])});
+    });
+
+    it('should replace certain values and stop recursion - maps - keys ', function() {
+        const target = {a: 1, b: new Map<any, any>([["a",1], [2, "x"], ["c", 3]])};
+
+        const result = visitReplace(target, (value, visitChilds, context) => {
+            return value === 2 ? 'replaced' : visitChilds(value, context)
+        });
+
+        expect(result).toStrictEqual({a: 1, b: new Map<any, any>([["a",1], ["replaced", "x"], ["c", 3]])});
+    });
+
+    it('should replace certain values and stop recursion - maps - keys 2', function() {
+        const target = {a: 1, b: new Map<any, any>([["a",1], [{x:2}, "y"], ["c", 3]])};
+
+        const result = visitReplace(target, (value, visitChilds, context) => {
+            return value === 2 ? 'replaced' : visitChilds(value, context)
+        });
+
+        expect(result).toStrictEqual({a: 1, b: new Map<any, any>([["a",1], [{x: "replaced"}, "y"], ["c", 3]])});
+    });
+
     it('should handle circular references correctly', function() {
         const target:any  = {a: 1, b: {c: 2}};
         target.d = target;  // Circular reference
--- common/util.ts
+++ common/util.ts
@@ -20,6 +20,11 @@ type VisitReplaceOptions = {
      * whether to pass on the context object. This hurts performance because the path is concatted every time, so use it only when needed. Setting this to "onError" re-executes the visitprelace with trackPath when an error was thrown
      */
     trackPath?: boolean | "onError",
+
+    /**
+     * Keep the order of sets and maps when replacing elements. Default: false
+     */
+    keepOrder?: boolean
 }
 
 /**
@@ -47,13 +52,78 @@ export function visitReplace<O extends object>(value: O, visitor: (value: unknow
             }
             visisitedObjects.add(obj);
 
-            for (let k in obj) {
-                const keyInParent = k as keyof object;
-                const childValue = obj[keyInParent];
-                let newValue = visitor(childValue, visitChilds, {...context, parentObject: value, key: keyInParent, diagnosis_path: (context.diagnosis_path !== undefined?`${context.diagnosis_path!}${diagnosis_jsonPath(keyInParent)}`:undefined)});
-                if(newValue !== childValue) { // Only if childValue really has changed. We don't want to interfer with setting a readonly property and trigger a proxy
-                    // @ts-ignore
-                    obj[keyInParent] = newValue;
+            if (value instanceof Set) {
+                let set = obj as Set<unknown>;
+                if (options.keepOrder) {
+                    throw new Error("Keep order not yet implemented");
+                }
+                for (const childValue of [...set.values()]) {
+                    let newValue = visitor(childValue, visitChilds, {
+                        ...context,
+                        parentObject: value,
+                        key: childValue,
+                        diagnosis_path: (context.diagnosis_path !== undefined ? context.diagnosis_path! : undefined)
+                    });
+
+                    if (newValue !== childValue) { // Only if childValue really has changed. We don't want to interfer with setting a readonly property and trigger a proxy
+                        // Replace:
+                        set.delete(childValue); set.add(newValue);
+                    }
+                }
+
+            }
+            else if (value instanceof Map) {
+                let map = obj as Map<unknown, unknown>;
+                if (options.keepOrder) {
+                    throw new Error("Keep order not yet implemented");
+                }
+
+                // Keys:
+                for (const key of [...map.keys()]) {
+                    let newKey = visitor(key, visitChilds, {
+                        ...context,
+                        parentObject: value,
+                        key: key,
+                        diagnosis_path: (context.diagnosis_path !== undefined ? `${context.diagnosis_path!}${diagnosis_jsonPath(key)}` : undefined)
+                    });
+
+                    if (newKey !== key) { // Only if key really has changed. We don't want to interfer with setting a readonly property and trigger a proxy
+                        // Replace key:
+                        let value = map.get(key);
+                        map.delete(key);
+                        map.set(newKey, value);
+                    }
+                }
+
+                // Values:
+                for (const [key, childValue] of [...map.entries()]) {
+                    let newValue = visitor(childValue, visitChilds, {
+                        ...context,
+                        parentObject: value,
+                        key: key,
+                        diagnosis_path: (context.diagnosis_path !== undefined ? `${context.diagnosis_path!}${diagnosis_jsonPath(key)}` : undefined)
+                    });
+
+                    if (newValue !== childValue) { // Only if childValue really has changed. We don't want to interfer with setting a readonly property and trigger a proxy
+                        map.set(key, newValue);
+                    }
+                }
+
+            }
+            else {
+                for (let k in obj) {
+                    const keyInParent = k as keyof object;
+                    const childValue = obj[keyInParent];
+                    let newValue = visitor(childValue, visitChilds, {
+                        ...context,
+                        parentObject: value,
+                        key: keyInParent,
+                        diagnosis_path: (context.diagnosis_path !== undefined ? `${context.diagnosis_path!}${diagnosis_jsonPath(keyInParent)}` : undefined)
+                    });
+                    if (newValue !== childValue) { // Only if childValue really has changed. We don't want to interfer with setting a readonly property and trigger a proxy
+                        // @ts-ignore
+                        obj[keyInParent] = newValue;
+                    }
                 }
             }
         }
